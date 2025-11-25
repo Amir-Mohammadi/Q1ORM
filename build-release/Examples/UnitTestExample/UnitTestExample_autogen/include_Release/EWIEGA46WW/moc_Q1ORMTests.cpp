ad7ffe857305032585f1a6aad7a26bf2cc75d21d
Amir-Mohammadi <amir.mhdi2019@gmail.com>
2025-11-25 21:39:54 +0330

    compelete all postgresql

---
+++ build-release/Examples/UnitTestExample/UnitTestExample_autogen/include_Release/EWIEGA46WW/moc_Q1ORMTests.cpp
@@ -0,0 +1,304 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'Q1ORMTests.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../../../../Examples/UnitTestExample/Q1ORMTests.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'Q1ORMTests.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Q1ORMTests_t {
+    QByteArrayData data[44];
+    char stringdata0[756];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Q1ORMTests_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Q1ORMTests_t qt_meta_stringdata_Q1ORMTests = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "Q1ORMTests"
+QT_MOC_LITERAL(1, 11, 12), // "initTestCase"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 15), // "cleanupTestCase"
+QT_MOC_LITERAL(4, 41, 4), // "init"
+QT_MOC_LITERAL(5, 46, 7), // "cleanup"
+QT_MOC_LITERAL(6, 54, 14), // "test_selectAll"
+QT_MOC_LITERAL(7, 69, 26), // "test_selectSpecificColumns"
+QT_MOC_LITERAL(8, 96, 23), // "test_selectAllCountries"
+QT_MOC_LITERAL(9, 120, 23), // "test_whereClause_simple"
+QT_MOC_LITERAL(10, 144, 27), // "test_whereClause_comparison"
+QT_MOC_LITERAL(11, 172, 15), // "test_orderByAsc"
+QT_MOC_LITERAL(12, 188, 16), // "test_orderByDesc"
+QT_MOC_LITERAL(13, 205, 18), // "test_customOrderBy"
+QT_MOC_LITERAL(14, 224, 10), // "test_limit"
+QT_MOC_LITERAL(15, 235, 21), // "test_limitWithOrderBy"
+QT_MOC_LITERAL(16, 257, 10), // "test_count"
+QT_MOC_LITERAL(17, 268, 19), // "test_countWithWhere"
+QT_MOC_LITERAL(18, 288, 8), // "test_max"
+QT_MOC_LITERAL(19, 297, 8), // "test_min"
+QT_MOC_LITERAL(20, 306, 8), // "test_sum"
+QT_MOC_LITERAL(21, 315, 8), // "test_avg"
+QT_MOC_LITERAL(22, 324, 18), // "test_distinctCount"
+QT_MOC_LITERAL(23, 343, 20), // "test_innerJoin_basic"
+QT_MOC_LITERAL(24, 364, 26), // "test_innerJoin_withAliases"
+QT_MOC_LITERAL(25, 391, 24), // "test_innerJoin_withWhere"
+QT_MOC_LITERAL(26, 416, 26), // "test_innerJoin_withOrderBy"
+QT_MOC_LITERAL(27, 443, 24), // "test_innerJoin_withLimit"
+QT_MOC_LITERAL(28, 468, 13), // "test_leftJoin"
+QT_MOC_LITERAL(29, 482, 14), // "test_rightJoin"
+QT_MOC_LITERAL(30, 497, 13), // "test_fullJoin"
+QT_MOC_LITERAL(31, 511, 12), // "test_groupBy"
+QT_MOC_LITERAL(32, 524, 22), // "test_groupByWithHaving"
+QT_MOC_LITERAL(33, 547, 21), // "test_include_showList"
+QT_MOC_LITERAL(34, 569, 21), // "test_include_showJson"
+QT_MOC_LITERAL(35, 591, 22), // "test_include_withWhere"
+QT_MOC_LITERAL(36, 614, 22), // "test_whereOrderByLimit"
+QT_MOC_LITERAL(37, 637, 26), // "test_joinWhereOrderByLimit"
+QT_MOC_LITERAL(38, 664, 11), // "test_toJson"
+QT_MOC_LITERAL(39, 676, 13), // "test_showJson"
+QT_MOC_LITERAL(40, 690, 11), // "test_toList"
+QT_MOC_LITERAL(41, 702, 20), // "test_toListWithWhere"
+QT_MOC_LITERAL(42, 723, 16), // "test_emptyResult"
+QT_MOC_LITERAL(43, 740, 15) // "test_nullValues"
+
+    },
+    "Q1ORMTests\0initTestCase\0\0cleanupTestCase\0"
+    "init\0cleanup\0test_selectAll\0"
+    "test_selectSpecificColumns\0"
+    "test_selectAllCountries\0test_whereClause_simple\0"
+    "test_whereClause_comparison\0test_orderByAsc\0"
+    "test_orderByDesc\0test_customOrderBy\0"
+    "test_limit\0test_limitWithOrderBy\0"
+    "test_count\0test_countWithWhere\0test_max\0"
+    "test_min\0test_sum\0test_avg\0"
+    "test_distinctCount\0test_innerJoin_basic\0"
+    "test_innerJoin_withAliases\0"
+    "test_innerJoin_withWhere\0"
+    "test_innerJoin_withOrderBy\0"
+    "test_innerJoin_withLimit\0test_leftJoin\0"
+    "test_rightJoin\0test_fullJoin\0test_groupBy\0"
+    "test_groupByWithHaving\0test_include_showList\0"
+    "test_include_showJson\0test_include_withWhere\0"
+    "test_whereOrderByLimit\0"
+    "test_joinWhereOrderByLimit\0test_toJson\0"
+    "test_showJson\0test_toList\0"
+    "test_toListWithWhere\0test_emptyResult\0"
+    "test_nullValues"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Q1ORMTests[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      42,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  224,    2, 0x08 /* Private */,
+       3,    0,  225,    2, 0x08 /* Private */,
+       4,    0,  226,    2, 0x08 /* Private */,
+       5,    0,  227,    2, 0x08 /* Private */,
+       6,    0,  228,    2, 0x08 /* Private */,
+       7,    0,  229,    2, 0x08 /* Private */,
+       8,    0,  230,    2, 0x08 /* Private */,
+       9,    0,  231,    2, 0x08 /* Private */,
+      10,    0,  232,    2, 0x08 /* Private */,
+      11,    0,  233,    2, 0x08 /* Private */,
+      12,    0,  234,    2, 0x08 /* Private */,
+      13,    0,  235,    2, 0x08 /* Private */,
+      14,    0,  236,    2, 0x08 /* Private */,
+      15,    0,  237,    2, 0x08 /* Private */,
+      16,    0,  238,    2, 0x08 /* Private */,
+      17,    0,  239,    2, 0x08 /* Private */,
+      18,    0,  240,    2, 0x08 /* Private */,
+      19,    0,  241,    2, 0x08 /* Private */,
+      20,    0,  242,    2, 0x08 /* Private */,
+      21,    0,  243,    2, 0x08 /* Private */,
+      22,    0,  244,    2, 0x08 /* Private */,
+      23,    0,  245,    2, 0x08 /* Private */,
+      24,    0,  246,    2, 0x08 /* Private */,
+      25,    0,  247,    2, 0x08 /* Private */,
+      26,    0,  248,    2, 0x08 /* Private */,
+      27,    0,  249,    2, 0x08 /* Private */,
+      28,    0,  250,    2, 0x08 /* Private */,
+      29,    0,  251,    2, 0x08 /* Private */,
+      30,    0,  252,    2, 0x08 /* Private */,
+      31,    0,  253,    2, 0x08 /* Private */,
+      32,    0,  254,    2, 0x08 /* Private */,
+      33,    0,  255,    2, 0x08 /* Private */,
+      34,    0,  256,    2, 0x08 /* Private */,
+      35,    0,  257,    2, 0x08 /* Private */,
+      36,    0,  258,    2, 0x08 /* Private */,
+      37,    0,  259,    2, 0x08 /* Private */,
+      38,    0,  260,    2, 0x08 /* Private */,
+      39,    0,  261,    2, 0x08 /* Private */,
+      40,    0,  262,    2, 0x08 /* Private */,
+      41,    0,  263,    2, 0x08 /* Private */,
+      42,    0,  264,    2, 0x08 /* Private */,
+      43,    0,  265,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Q1ORMTests::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Q1ORMTests *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->initTestCase(); break;
+        case 1: _t->cleanupTestCase(); break;
+        case 2: _t->init(); break;
+        case 3: _t->cleanup(); break;
+        case 4: _t->test_selectAll(); break;
+        case 5: _t->test_selectSpecificColumns(); break;
+        case 6: _t->test_selectAllCountries(); break;
+        case 7: _t->test_whereClause_simple(); break;
+        case 8: _t->test_whereClause_comparison(); break;
+        case 9: _t->test_orderByAsc(); break;
+        case 10: _t->test_orderByDesc(); break;
+        case 11: _t->test_customOrderBy(); break;
+        case 12: _t->test_limit(); break;
+        case 13: _t->test_limitWithOrderBy(); break;
+        case 14: _t->test_count(); break;
+        case 15: _t->test_countWithWhere(); break;
+        case 16: _t->test_max(); break;
+        case 17: _t->test_min(); break;
+        case 18: _t->test_sum(); break;
+        case 19: _t->test_avg(); break;
+        case 20: _t->test_distinctCount(); break;
+        case 21: _t->test_innerJoin_basic(); break;
+        case 22: _t->test_innerJoin_withAliases(); break;
+        case 23: _t->test_innerJoin_withWhere(); break;
+        case 24: _t->test_innerJoin_withOrderBy(); break;
+        case 25: _t->test_innerJoin_withLimit(); break;
+        case 26: _t->test_leftJoin(); break;
+        case 27: _t->test_rightJoin(); break;
+        case 28: _t->test_fullJoin(); break;
+        case 29: _t->test_groupBy(); break;
+        case 30: _t->test_groupByWithHaving(); break;
+        case 31: _t->test_include_showList(); break;
+        case 32: _t->test_include_showJson(); break;
+        case 33: _t->test_include_withWhere(); break;
+        case 34: _t->test_whereOrderByLimit(); break;
+        case 35: _t->test_joinWhereOrderByLimit(); break;
+        case 36: _t->test_toJson(); break;
+        case 37: _t->test_showJson(); break;
+        case 38: _t->test_toList(); break;
+        case 39: _t->test_toListWithWhere(); break;
+        case 40: _t->test_emptyResult(); break;
+        case 41: _t->test_nullValues(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Q1ORMTests::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_Q1ORMTests.data,
+    qt_meta_data_Q1ORMTests,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Q1ORMTests::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Q1ORMTests::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Q1ORMTests.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Q1ORMTests::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 42)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 42;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 42)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 42;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
--- build/Desktop_Qt_5_15_2_MSVC2019_64bit-Release/Tools/ExampleProjectModifier/CMakeFiles/ExampleProjectModifier.dir/icon.i
+++
@@ -1,2 +0,0 @@
-#line 1 "C:\\Users\\ARM\\Desktop\\Q1ORM\\Tools\\ExampleProjectModifier\\icon.rc"
-IDI_ICON1 ICON "icon.ico"
