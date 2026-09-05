6cd93570c0281650b1a71580235b901f043d1082
Amir-Mohammadi <amir.mhdi2019@gmail.com>
2026-09-05 22:36:24 +0330

    delete unnecessery files and folders

--- build/final_scan/Examples/MySqlTestExample/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ARM/Desktop/Q1ORM/Examples/MySqlTestExample
-# Build directory: C:/Users/ARM/Desktop/Q1ORM/build/final_scan/Examples/MySqlTestExample
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
